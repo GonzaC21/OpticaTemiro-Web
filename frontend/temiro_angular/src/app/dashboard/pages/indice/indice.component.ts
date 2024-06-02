f39d23b29269a0073d95032bf18afec9b5f9b5af
Urbina Tatiana <urbinatatiana077@gmail.com>
2024-06-02 16:13:09 -0300

    se realiza la protección de ruta del dashboard de usuario

--- frontend/temiro_angular/src/app/dashboard/pages/indice/indice.component.ts
+++ frontend/temiro_angular/src/app/dashboard/pages/indice/indice.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-indice',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class IndiceComponent {
 
+  router = inject(Router);
+
+  onClickSalir(){
+    localStorage.removeItem('usuario');
+    this.router.navigate(['/login']);
+  }
+
+
 }
+
+
+
+
